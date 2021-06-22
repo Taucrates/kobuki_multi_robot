4aef5f8ed8761051c83e42c83db02dd4316dbc98
esauortiz <esauortiz97@gmail.com>
2021-06-22 08:43:00 +0200

    add start and nav bash files

---
+++ documentation/start_and_nav/kobuki_c/kobuki_navigation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+roslaunch kobuki_multi_robot kobuki_navigation.launch init_pose_x:=0 init_pose_y:=0 kobuki_id:=3 robot_name:=kobuki_c
---
+++ documentation/start_and_nav/kobuki_c/kobuki_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+roslaunch kobuki_multi_robot kobuki.launch robot_name:=kobuki_c sensor:=hokuyo04LX
