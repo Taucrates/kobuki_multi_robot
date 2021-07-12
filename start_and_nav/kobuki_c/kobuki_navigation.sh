9714f1a956758bcdfbbf773dbab8c6f2ab534590
esauortiz <esauortiz97@gmail.com>
2021-07-12 10:06:28 +0200

    update start and nav bash files

--- start_and_nav/kobuki_c/kobuki_navigation.sh
+++ start_and_nav/kobuki_c/kobuki_navigation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-roslaunch kobuki_multi_robot kobuki_navigation.launch init_pose_x:=0 init_pose_y:=0 kobuki_id:=3 robot_name:=kobuki_c
+roslaunch kobuki_multi_robot kobuki_navigation.launch init_pose_x:=5 init_pose_y:=0 kobuki_id:=3 robot_name:=kobuki_c
